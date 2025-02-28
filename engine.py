fb23740626e7e899ac6217bffa0abca60647adf8
rostik277 <kaiirsrorstv@gmail.com>
2025-02-28 20:41:21 +0200

    1.1R

--- engine.py
+++ engine.py
@@ -1,12 +1,27 @@
 import sqlite3
 
-with sqlite3.connect('database.db') as db:
-    cur = db.cursor()
-    cur.execute("SELECT * FROM targets")
+def attack():
+    request1 = input('Введіть чим вєбати (номер):')
+    request2 = input('Введіть снаряд яким вєбати:')
+    request3 = input('Введіть широта куди потрібно вєбати:')
+    request4 = input('Введіть довготу куди потрібно вєбати:')
+    conn = sqlite3.connect('database.db')
+    cursor = conn.cursor()
 
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
+    query = """
+    SELECT COUNT(*) FROM targets WHERE field3 = ? AND field4 = ?
+    """
+    cursor.execute(query, (request3, request4))
+    result = cursor.fetchone()
 
-# test 123
+    if result[0] > 0:
+        print("Координати знайдено в базі даних.")
+        return True
+    else:
+        print("Координати не знайдено в базі даних.")
+        return False
+
+    conn.close()
+
+attack()
 
